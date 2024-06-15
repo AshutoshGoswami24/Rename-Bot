72460eff2c4a392e512e4f8941664a6e210f2337
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-15 16:05:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,10 @@ class Config(object):
     # channels logs
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    
 
     # wes response configuration     
+    PORT = int(os.environ.get("PORT", ""))
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 
 #ʀᴀᴘᴏ ᴄʀᴇᴀᴛᴏʀ https://github.com/AshutoshGoswami24
